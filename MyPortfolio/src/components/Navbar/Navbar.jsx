88e0c9b810f3713bea0489ba4bdd80f06613b3ea
Simran Verma <vermasimran327@gmail.com>
2026-01-16 15:19:12 +0530

    navbar update

--- MyPortfolio/src/components/Navbar/Navbar.jsx
+++ MyPortfolio/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import {FiMenu,FiX} from 'react-icons/fi'
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
+
 
 const Navbar = () => {
+
+
+  const menuItems =[
+    {id:"about", label:"About"},
+    {id:"skills", label:"Skills"},
+    {id:"education", label:"Education"},
+    // {id:"contact", label:"Contact"},
+    {id:"experience", label:"Experience"},
+    {id:"work", label:"Work"},];
+    
   return (
-    <div>Navbar</div>
+    <nav className="bg-transparent">
+     <div className='text-white py-5 flex justify-between items-center'>
+      <div className='text-5xl font-semibold relative cursor-pointer'>
+      <span className='text-[#8245ec]'>&lt;</span>
+      <span className='text-white'>Simran</span>
+      <span className='text-[#8245ec]'>/</span>
+      <span className='text-[#8245ec]'>Verma</span>
+      <span className='text-[#8245ec]'>&gt;</span>
+      </div>
+
+       <ul className='md:flex space-x-8 text-gray-300'>
+
+        {menuItems.map((item) =>{
+          <li key ={item.id} className=" ">
+            <button>
+              {item.label}
+            </button>
+          </li>
+        })}
+       </ul>
+
+        </div>
+    </nav>
   )
 }
 
--- MyPortfolio/tailwind.config.js
+++ MyPortfolio/tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
